7d5f93aae8b963bcbe4ee8b1e87f831d273fe3ee
dmallia17 <Daniel.Mallia21@myhunter.cuny.edu>
2020-01-20 15:58:23 -0500

    Add EAST

--- char.py
+++ char.py
@@ -3,8 +3,16 @@
 # Use of EAST detector inspired by: 
 # https://www.pyimagesearch.com/2018/08/20/opencv-text-detection-east-text-detector/
 
-import cv2
+# NOTE: This script uses the TensorFlow Implementation of the EAST text 
+# detector, which is offered by OpenCV. 
+# The actual Github for this implementation is here: 
+# https://github.com/argman/EAST
+# The download link for the .pb file (trained model) is here: 
+# https://www.dropbox.com/s/r2ingd0l3zt8hxs/frozen_east_text_detection.tar.gz?dl=1
+
+import cv as cv
 import numpy as np 
+import copy
 
 def findCorners(bound):
     c1 = [bound[3][0],bound[0][1]]
@@ -14,19 +22,33 @@ def findCorners(bound):
     return [c1,c2,c3,c4]
 
 def returnWordBBoxes(image):
+    output = copy.deepcopy(image)
 
+    # Need to scale image to a multiple of 32 - just steps down to next multiple of 32 
+    newRows, newCols = output.shape[:2]
+    newRows -= newRows % 32
+    newCols -= newCols % 32
+    output = cv.resize(image, (newRows , newCols), interpolation = cv.INTER_LINEAR)
+
+    # Import pretrained EAST detector
+    east = cv.dnn.readNet('frozen_east_text_detection.pb')
+
+    # Required layer names
+    layerNames = [ "feature_fusion/Conv_7/Sigmoid", "feature_fusion/concat_3"]
+
+    
 
 if __name__ == "__main__":
 
     bndingBx = []#holds bounding box of each countour
     corners = []
-    image = cv2.imread('e.png',0)
-    blur = cv2.GaussianBlur(image,(5,5),0)
-    threshold, threshImage = cv2.threshold(blur,0,255,
-        cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    image = cv.imread('e.png',0)
+    blur = cv.GaussianBlur(image,(5,5),0)
+    threshold, threshImage = cv.threshold(blur,0,255,
+        cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
      
-    contours, heirar = cv2.findContours(threshImage, cv2.RETR_CCOMP, 
-        cv2.CHAIN_APPROX_SIMPLE)
+    contours, heirar = cv.findContours(threshImage, cv.RETR_CCOMP, 
+        cv.CHAIN_APPROX_SIMPLE)
     for num in range(0,len(contours)):
         #make sure contour is for letter and not cavity
         if(heirar[0][num][3] == -1):
@@ -40,9 +62,9 @@ if __name__ == "__main__":
             corners.append(findCorners(bx))
     
         #draw the countours on threshImage image
-        x,y,w,h = cv2.boundingRect(threshImage)
+        x,y,w,h = cv.boundingRect(threshImage)
 
-    cv2.imshow('thresh', threshImage)
-    cv2.waitKey()
+    cv.imshow('thresh', threshImage)
+    cv.waitKey()
 
 
